24fc8dff9e9e8a0dc157b1998df0ab94c7da03ac
adiaguidry <adiaguidry@gmail.com>
2023-04-19 10:44:09 -0600

    added crud functions to chores

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import PendingChore, ChoreDetail
+from .views import PendingChore, ChoreDetail, CreateChore, EditChore, DeleteChore
 
 
 urlpatterns = [
-    path('', PendingChore.as_view(), name='pending'),
-    path('chore/<int:pk>', ChoreDetail.as_view(), name='chore')
+    path('', PendingChore.as_view(), name='chores'),
+    path('chore/<int:pk>', ChoreDetail.as_view(), name='chore'),
+    path('create-chore/', CreateChore.as_view(), name='create-chore'),
+    path('edit-chore/<int:pk>', EditChore.as_view(), name='edit-chore'),
+    path('delete-chore/<int:pk>', DeleteChore.as_view(), name='delete-chore'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Chore
 
 # Create your views here.
@@ -11,4 +13,22 @@ class ChoreDetail(DetailView):
     model = Chore
     context_object_name = 'chore'
     template_name = 'base/chore.html'
+
+class CreateChore(CreateView):
+    model = Chore
+    template_name = 'base/chore_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('chores')
+
+class EditChore(UpdateView):
+    model = Chore
+    template_name = 'base/chore_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('chores')
+
+class DeleteChore(DeleteView):
+    model = Chore
+    context_object_name = 'chore'
+    template_name = 'base/delete_chore.html'
+    success_url = reverse_lazy('chores')
 
